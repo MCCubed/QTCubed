c98efd47fbfec77f34f96bc86a9cc8b35772a963
Charles Chappell <shadow@mc-cubed.net>
2010-02-22 02:34:26 +0000

    Updated license text boilerplate to reflect new licensing status

--- src/java/net/mc-cubed/qtcubed/QTMovie.java
+++ src/java/net/mc-cubed/qtcubed/QTMovie.java
@@ -5,15 +5,27 @@
 //  Created by Chappell Charles on 10/02/19.
 //  Copyright (c) 2010 MC Cubed, Inc. All rights reserved.
 //
-//  This is proprietary software, and may not be used without the express written consent of:
+//  This program is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation; either version 2 of the License, or
+//  (at your option) any later version.
+//  
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//  
+//  You should have received a copy of the GNU General Public License along
+//  with this program; if not, write to the Free Software Foundation, Inc.,
+//  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+//  
+//  
+//  This is also dual licensed as proprietary software, and may be used in
+//  commercial/proprietary software products by obtaining a license from:
 //  MC Cubed, Inc
 //  1-3-4 Kamikizaki, Urawa-ku
 //  Saitama, Saitama, 330-0071
 //  Japan
-//
-//  このソフトウェアは専売権付ソフトウェアです。株式会社MCキューブの許可なく複製、転用、販売等の二次利用をすることを固く禁じます。
-//  株式会社エムシーキューブ
-//  埼玉県さいたま市浦和区上木崎１−３−４
 
 package net.mc_cubed.qtcubed;
 
